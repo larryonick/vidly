67c3fcd112a6bb0b00a56bdc2f9ffa3f429e0241
Lanre Fasuyi <lanrefasuyii@gmail.com>
2020-01-25 03:16:46 +0100

    Created the Category listing Component

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 
 const ListGroup = (props) => {
+  console.log('props', props);
   const { items } = props;
   return (
     <ul className="list-group">
-      {items.map(item => {
+      {items.map(item => (
         <li key={item._id} className="list-group-item">
           {item.name}
         </li>
-      })}
+      ))}
     </ul>
   );
 }
